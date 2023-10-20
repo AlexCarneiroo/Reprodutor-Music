6cc7f0ebdefc1150d805e4d88e8f15aea600b8f9
AlexCarneiroo <121904833+AlexCarneiroo@users.noreply.github.com>
2023-10-20 14:04:59 -0300

    Alguns Ajustes

--- script.js
+++ script.js
@@ -4,30 +4,43 @@ const duration = document.querySelector('.duration')
 const btn = document.querySelector('.btn')
 const spanAdd = document.getElementById('spanAdd')
 const todosBtn = document.querySelectorAll('button')
-todosBtn.forEach(botao => {
-    botao.addEventListener('click' , ()=>{
-        let timerOut;
+const volumeRange = document.getElementById('volumeRange')
+let rotationInteval
 
-        if(botao.classList.contains('teste')){
-            clearTimeout(timerOut)
-        }else{
-            botao.classList.add('teste')
-            timerOut = setTimeout(()=>{
-                botao.classList.remove('teste')
-            },1000)
-        }
-    })
-})
+
+
+
+
+
+const rotation = ()=>{
+    const elemento = document.querySelector('.areaLogo')
+    rotationInteval = setInterval(()=>{
+        let rotation = getComputedStyle(elemento).getPropertyValue('--rotation') || 0;
+        rotation = parseInt(rotation, 10);
+        rotation += 1;
+        elemento.style.setProperty('--rotation', rotation);
+        elemento.style.transform = `rotate(${rotation}deg)`;
+    },10)
+}
+
+
 
 inputFile.addEventListener('change' , (e)=>{
     const inputTarge = e.target
     const file = inputTarge.files[0]
     const btnPause = document.querySelector('#pause')
+    
+
     if(file){
         dadosMusic.innerHTML = file.name
         const url = URL.createObjectURL(file)
-    
         const audio = new Audio(url)
+
+        const ajustandoVolume = () =>{
+            const volumeValue = parseFloat(volumeRange.value) / 100;
+             audio.volume = volumeValue
+        }
+        volumeRange.addEventListener('input' , ajustandoVolume)
         
         audio.addEventListener('loadedmetadata' , ()=>{
             const minutos = Math.floor(audio.duration / 60)
@@ -38,11 +51,14 @@ inputFile.addEventListener('change' , (e)=>{
             audio.play()
             btn.style.display ='none'
             btnPause.style.display='flex'
+            rotation()
         })
         btnPause.addEventListener('click' , ()=>{
             audio.pause()
             btnPause.style.display ='none'
             btn.style.display ='flex'
+
+            clearInterval(rotationInteval)
         })
         inputFile.disabled = true
         spanAdd.classList.add('spanAdd')
